d469128097a24e2fdf5bec87e001ac61a5b673a6
Jakkestt <41551796+Jakkestt@users.noreply.github.com>
2026-02-15 22:26:54 +0200

    Create radius_clip.rs

---
+++ examples/ui/scroll_and_overflow/radius_clip.rs
@@ -0,0 +1,84 @@
+use bevy::{
+    color::palettes::css::{BLUE, RED},
+    prelude::*,
+};
+
+#[derive(Resource)]
+struct Marker(Entity);
+
+fn main() {
+    let mut app = App::new();
+
+    app.add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .add_systems(Update, update);
+
+    app.run();
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2d);
+    commands
+        .spawn(
+            (Node {
+                width: percent(100),
+                height: percent(100),
+                align_items: AlignItems::Center,
+                justify_content: JustifyContent::Center,
+                ..Default::default()
+            }),
+        )
+        .with_children(|parent| {
+            let id = parent
+                .spawn((
+                    Node {
+                        width: px(100),
+                        height: px(100),
+                        border: UiRect::all(px(10)),
+                        border_radius: BorderRadius::MAX,
+                        overflow: Overflow::clip(),
+                        align_items: AlignItems::Center,
+                        justify_content: JustifyContent::Center,
+                        ..Default::default()
+                    },
+                    BorderColor::all(BLUE),
+                ))
+                .with_children(|parent| {
+                    parent.spawn((
+                        Node {
+                            width: percent(110),
+                            height: percent(110),
+                            ..Default::default()
+                        },
+                        BackgroundColor(RED.into()),
+                    ));
+                })
+                .id();
+
+            parent.commands_mut().insert_resource(Marker(id));
+        });
+}
+
+fn update(mut nodes: Query<&mut Node>, marker: Res<Marker>, keys: Res<ButtonInput<KeyCode>>) {
+    let Ok(mut node) = nodes.get_mut(marker.0) else {
+        return;
+    };
+
+    if keys.just_pressed(KeyCode::KeyT) {
+        node.overflow = match node.overflow {
+            Overflow {
+                x: OverflowAxis::Visible,
+                y: OverflowAxis::Visible,
+            } => Overflow::clip_y(),
+            Overflow {
+                x: OverflowAxis::Visible,
+                y: OverflowAxis::Clip,
+            } => Overflow::clip_x(),
+            Overflow {
+                x: OverflowAxis::Clip,
+                y: OverflowAxis::Visible,
+            } => Overflow::clip(),
+            _ => Overflow::visible(),
+        };
+    }
+}
